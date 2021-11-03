f3bc24264cb9c5822a4f4ffe9d668424145baab6
Roy Rasugu <roy.rasugu@student.moringaschool.com>
2021-11-03 05:29:48 +0300

    register thr models

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from hood.models import Neighbourhood, Post, Profile, Business, Health, Police
 
 # Register your models here.
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(Profile)
+admin.site.register(Post)
+admin.site.register(Health)
+admin.site.register(Police)
